0b57be9bfd4e6e1aa0ed4daded4451744c9723ab
Your Full Name <your-email@example.com>
2026-03-12 08:21:00 -0700

    Initial starter code for Lab 2

---
+++ src/fa/com/lab2/Main.java
@@ -0,0 +1,14 @@
+package fa.com.lab2;
+
+/**
+ * Entry point for Практика 2: Основные конструкции языка Java.
+ * Use this class to run and test your task implementations.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Практика 2: Основные конструкции языка Java");
+        // TODO: Call Task1 and Task2 methods here to test your solutions
+        System.out.println("Task1.max(3, 7) = " + Task1.max(3, 7));
+        System.out.println("Task2.isPositive(5) = " + Task2.isPositive(5));
+    }
+}
---
+++ src/fa/com/lab2/Task1.java
@@ -0,0 +1,9 @@
+package fa.com.lab2;
+
+public class Task1 {
+    // TODO: Implement a method that returns the maximum of two integers.
+    public static int max(int a, int b) {
+        // Your code here
+        return 0;
+    }
+}
